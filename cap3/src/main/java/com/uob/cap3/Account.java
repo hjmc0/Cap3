e974633590f3a1322dbff390211a2b8b6c64a0ff
ljinan <87352325+ljinan@users.noreply.github.com>
2023-10-20 10:50:44 +0800

    update account repo and entity

---
+++ cap3/src/main/java/com/uob/cap3/Account.java
@@ -0,0 +1,21 @@
+package com.uob.cap3;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+}
---
+++ cap3/src/main/java/com/uob/cap3/AccountRepo.java
@@ -0,0 +1,9 @@
+package com.uob.cap3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepo extends JpaRepository<{
+    
+}

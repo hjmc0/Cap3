3eba6a89fff7abacc7052292bdd33d03035e0004
hjmc0 <hjmc0@outlook.com>
2023-10-20 10:29:59 +0800

    Initialise Spring Project

---
+++ cap3/src/main/java/com/uob/cap3/Cap3Application.java
@@ -0,0 +1,13 @@
+package com.uob.cap3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cap3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cap3Application.class, args);
+	}
+
+}

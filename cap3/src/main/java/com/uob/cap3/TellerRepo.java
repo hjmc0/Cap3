f8802f377b061aafd0a7dc1c0465c3d4e59b0384
ljinan <87352325+ljinan@users.noreply.github.com>
2023-10-20 10:59:16 +0800

    Update teller and tellerrepo

---
+++ cap3/src/main/java/com/uob/cap3/TellerRepo.java
@@ -0,0 +1,7 @@
+package com.uob.cap3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TellerRepo extends JpaRepository<Teller,Long>{
+    
+}
--- cap3/src/main/java/com/uob/cap3/TransactionRepo.java
+++ cap3/src/main/java/com/uob/cap3/TransactionRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepo extends JpaRepository<Account,Long>{
+public interface TransactionRepo extends JpaRepository<Transaction, Long> {
     
 }

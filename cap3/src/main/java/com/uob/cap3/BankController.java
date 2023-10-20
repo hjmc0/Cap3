6272ddcda00b12ded953cedac019f4213c81ae00
ljinan <87352325+ljinan@users.noreply.github.com>
2023-10-20 16:03:40 +0800

    Update transact and controller

--- cap3/src/main/java/com/uob/cap3/BankController.java
+++ cap3/src/main/java/com/uob/cap3/BankController.java
@@ -1,11 +1,21 @@
 package com.uob.cap3;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.uob.cap3.entities.Account;
+import com.uob.cap3.repo.AccountRepo;
 
 @Controller
 public class BankController {
+
+    @Autowired
+    AccountRepo ar;
     
     @RequestMapping("/login")
     public String loginPage(){
@@ -17,14 +27,9 @@ public class BankController {
         return "view";
     }
 
-    @RequestMapping("/withdraw/{id}")
-    public String withdrawPage(@PathVariable int id){
-        return "inout";
-    }
-
-    @RequestMapping("/deposit/{id}")
-    public String depositPage(@PathVariable int id){
-        return "inout";
+    @RequestMapping("/transact/{id}")
+    public String transact(){
+        return "transact";
     }
 
     @RequestMapping("/transaction/{id}")
@@ -40,5 +45,34 @@ public class BankController {
     @RequestMapping("/createaccount")
     public String createAccount(){
         return "createaccount";
+    }
+    
+    @GetMapping("/savetransact")
+    public String saveTransact(@RequestParam("id") Long id, @RequestParam("choice") String choice, @RequestParam("amt")Long amt, Model m) {
+        Account acc = new Account();
+        double bal = ar.findbalance(id);
+        String results = "";
+        
+        if(choice.equals("deposit")) {
+                bal = bal + amt;
+                acc.setBalance(bal);
+                ar.save(acc);
+                results = "redirect:/list";
+        } else {
+            boolean toContinue = true;
+            while(toContinue) {
+                if(amt > bal) {
+                    results = "redirect:/transact";
+                } else {
+                    bal = bal - amt;
+                    acc.setBalance(bal);
+                    ar.save(acc);
+                    results = "redirect:/list";
+                    toContinue = false;
+                }
+            }
+        }
+        return results;
+       
     }
 }
--- cap3/src/main/java/com/uob/cap3/repo/AccountRepo.java
+++ cap3/src/main/java/com/uob/cap3/repo/AccountRepo.java
@@ -1,6 +1,7 @@
 package com.uob.cap3.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.uob.cap3.entities.Account;
@@ -8,4 +9,6 @@ import com.uob.cap3.entities.Account;
 @Repository
 public interface AccountRepo extends JpaRepository<Account,Long>{
     
+    @Query(value = "select balance from account where account_id = :aid", nativeQuery = true)
+    double findbalance(long aid);
 }

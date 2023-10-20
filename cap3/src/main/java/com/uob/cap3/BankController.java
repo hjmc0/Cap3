400b1e24d47b51f873ce8f00bc0d0029f287c49d
shurui99 <shurui9910@gmail.com>
2023-10-20 15:40:06 +0800

    Updated landing page

--- cap3/src/main/java/com/uob/cap3/BankController.java
+++ cap3/src/main/java/com/uob/cap3/BankController.java
@@ -6,39 +6,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BankController {
-    
+
+    @RequestMapping("/")
+    public String landing() {
+        return "index";
+    }
+
     @RequestMapping("/login")
-    public String loginPage(){
+    public String loginPage() {
         return "login";
     }
 
     @RequestMapping("/view")
-    public String viewPage(){
+    public String viewPage() {
         return "view";
     }
 
     @RequestMapping("/withdraw/{id}")
-    public String withdrawPage(@PathVariable int id){
+    public String withdrawPage(@PathVariable int id) {
         return "inout";
     }
 
     @RequestMapping("/deposit/{id}")
-    public String depositPage(@PathVariable int id){
+    public String depositPage(@PathVariable int id) {
         return "inout";
     }
 
     @RequestMapping("/transaction/{id}")
-    public String transactionPage(@PathVariable int id){
+    public String transactionPage(@PathVariable int id) {
         return "transaction";
     }
 
     @RequestMapping("/createteller")
-    public String createTeller(){
+    public String createTeller() {
         return "createteller";
     }
 
     @RequestMapping("/createaccount")
-    public String createAccount(){
+    public String createAccount() {
         return "createaccount";
     }
 }

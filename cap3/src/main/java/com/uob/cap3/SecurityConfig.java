990ec5b144d174d45bbd3c5cf1e32aacf5b8c324
zhengxiangxp <54161150+zhengxiangxp@users.noreply.github.com>
2023-10-25 14:55:05 +0800

    Security

--- cap3/src/main/java/com/uob/cap3/SecurityConfig.java
+++ cap3/src/main/java/com/uob/cap3/SecurityConfig.java
@@ -1,29 +1,20 @@
 package com.uob.cap3;
 
-// import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // @Bean
-    // UserDetailsService userDetailsService(PasswordEncoder encoder){
-    // UserDetails admin =
-    // User.withUsername("admin").password(encoder.encode("pass")).roles("ADMIN").build();
-    // return new InMemoryUserDetailsManager(admin);
-    // }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {

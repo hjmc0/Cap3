df7ff679be79b1e6b3db54074ed748ec129676ce
hjmc0 <hjmc0@outlook.com>
2023-10-26 20:28:09 +0800

    Create cap3.sql

---
+++ cap3/cap3.sql
@@ -0,0 +1,17 @@
+insert into teller(teller_name, teller_pass) values('john', '$2a$12$IFwNku56cjhjFdDe9/6XSe/K9zNJAfcz.VhkU7u0bjy9KHX9cpPlm');
+insert into role(role_name) values('superadmin');
+insert into role(role_name) values('admin');
+insert into role(role_name) values('teller');
+insert into role_teller values(1,1);
+
+insert into account(balance, account_name, address, email, phone, status) values(100.20, 'Sam Smith', 'Yishun 404', 'sam@gmail.com', '90909991','active');
+insert into account(balance, account_name, address, email, phone, status) values(1000.20, 'Peter Parker', 'New York City Street 111', 'PeePee@gmail.com', '80901191','active');
+insert into account(balance, account_name, address, email, phone, status) values(200.15, 'Superman Batman', 'Gotham Bat Cave', 'iAmBatMan@gmail.com', '999111','active');
+insert into account(balance, account_name, address, email, phone, status) values(3560.27, 'Sridhar Turner', 'Somewhere on Planet Earth', 'SriSri@gmail.com', '80809011','active');
+insert into account(balance, account_name, address, email, phone, status) values(1010.20, 'Eddie Hall', 'Muscle Beach', 'StrongestMan@gmail.com', '80908011','active');
+insert into account(balance, account_name, address, email, phone, status) values(10220.27, 'Marcus', 'Redhill Street 7', 'marcus90@gmail.com', '88911192','active');
+insert into account(balance, account_name, address, email, phone, status) values(1100.20, 'ShuRui', 'Somewhere at Topayo', 'shurui99@gmail.com', '84901191','active');
+insert into account(balance, account_name, address, email, phone, status) values(7100.20, 'Jin An', 'Alpha Street 11', 'jinanan@gmail.com', '80913012','active');
+insert into account(balance, account_name, address, email, phone, status) values(90100.20, 'Chandra', 'Planet Chandra', 'chanchan@gmail.com', '84009011','active');
+insert into account(balance, account_name, address, email, phone, status) values(31200.20, 'Meat Head', 'Meat Planet', 'MeatMan@gmail.com', '80114011','active');            
+insert into account(balance, account_name, address, email, phone, status) values(31200.20, 'Susan', 'NTUC Learn Hub', 'Susu@gmail.com', '90414011','active');
